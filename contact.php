35bca8d7ccf3501f68d83f16cc8df89688d52b26
Sunny Ojo <sunny.ojo.14@gmail.com>
2020-05-23 17:10:14 +0100

    updated contact.php

--- contact.php
+++ contact.php
@@ -6,7 +6,9 @@ $from = $_POST["email"];
 $name = $_POST["name"];
 $subject = $_POST["subject"];
 $number = $_POST["number"];
-$cmessage = $_POST["message"];
+$cmessage = $_POST["your_message"];
+$company = $_POST["company_name"];
+$companyadd = $_POST["company_address"];
 
 $headers = "From: $from";
 $headers = "From: " . $from . "\r\n";
@@ -28,6 +30,10 @@ $body .= "<td style='border:none;'><strong>Name:</strong> {$name}</td>";
 $body .= "<td style='border:none;'><strong>Email:</strong> {$from}</td>";
 $body .= "</tr>";
 $body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$subject}</td></tr>";
+$body .= "<td style='border:none;'><strong>Company Name:</strong> {$company}</td>";
+$body .= "<td style='border:none;'><strong>Company Address:</strong> {$companyadd}</td>";
+
+
 $body .= "<tr><td></td></tr>";
 $body .= "<tr><td colspan='2' style='border:none;'>{$cmessage}</td></tr>";
 $body .= "</tbody></table>";
