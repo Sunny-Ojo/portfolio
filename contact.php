878b9b63729c40ff49960fd0ffafbf8f1d0cb69e
Sunny Ojo <sunny.ojo.14@gmail.com>
2020-05-23 17:05:14 +0100

    updated contact.php

--- contact.php
+++ contact.php
@@ -1,29 +1,43 @@
 <?php
-session_start();
 
-// require 'vendor/autoload.php'; // If you're using Composer (recommended)
-// Comment out the above line if not using Composer
-require "sendgrid-php.php";
-// If not using Composer, uncomment the above line and
-// download sendgrid-php.zip from the latest release here,
-// replacing <PATH TO> with the path to the sendgrid-php.php file,
-// which is included in the download:
-// https://github.com/sendgrid/sendgrid-php/releases
+$to = "njokusunnyojo@gmail.com";
+$from = $_POST["email"];
+$name = $_POST["name"];
+$subject = $_POST["subject"];
+$number = $_POST["number"];
+$cmessage = $_POST["message"];
 
-$email = new \SendGrid\Mail\Mail();
-$email->setFrom($_POST["email"]);
-$email->setSubject("Sending with Twilio SendGrid is Fun");
-$email->addTo("njokusunnyojo@gmail.com");
-$email->addContent("text/plain", "and easy to do anywhere, even with PHP");
-$email->addContent(
-    "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
-);
-$sendgrid = new \SendGrid('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');
-try {
-    $response = $sendgrid->send($email);
-    print $response->statusCode() . "\n";
-    print_r($response->headers());
-    print $response->body() . "\n";
-} catch (Exception $e) {
-    echo 'Caught exception: ' . $e->getMessage() . "\n";
+$headers = "From: $from";
+$headers = "From: " . $from . "\r\n";
+$headers .= "Reply-To: " . $from . "\r\n";
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+$subject = "You have a message from your portfolio.";
+
+$logo = 'img/logo.png';
+$link = '#';
+
+$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Express Mail</title></head><body>";
+$body .= "<table style='width: 100%;'>";
+$body .= "<thead style='text-align: center;'><tr><td style='border:none;' colspan='2'>";
+$body .= "<a href='{$link}'><img src='{$logo}' alt=''></a><br><br>";
+$body .= "</td></tr></thead><tbody><tr>";
+$body .= "<td style='border:none;'><strong>Name:</strong> {$name}</td>";
+$body .= "<td style='border:none;'><strong>Email:</strong> {$from}</td>";
+$body .= "</tr>";
+$body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$subject}</td></tr>";
+$body .= "<tr><td></td></tr>";
+$body .= "<tr><td colspan='2' style='border:none;'>{$cmessage}</td></tr>";
+$body .= "</tbody></table>";
+$body .= "</body></html>";
+ini_set("SMTP", "ssl://smtp.gmail.com");
+ini_set("smtp_port", "587");
+$send = mail($to, $subject, $body, $headers);
+if ($send) {
+    $_SESSION["success"] = 'Sent';
+    header("Location:index.php");
+} else {
+    $_SESSION["error"] = 'Sorry, error occured.';
+    header("Location:index.php");
 }
